414e72a6a69914023382ac4bfe25c965d9b956a8
Tapaswalde <tapaswalde@gmail.com>
2026-02-13 21:05:59 +0530

    Todo Application

---
+++ TODO/backend/server.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+const app=express();
+
+//basic middleware
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.listen(5000,()=>{
+    console.log('Dog is running on port 5000');
+})
+
+module.exports=app;
