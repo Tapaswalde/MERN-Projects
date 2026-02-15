f6b3973c1660b04c3b34e34d8527e5cbe537f07e
Tapaswalde <tapaswalde@gmail.com>
2026-02-16 00:07:47 +0530

    new changes

--- TODO/backend/controllers/userController.js
+++ TODO/backend/controllers/userController.js
@@ -1,5 +1,6 @@
 const userModel = require("../model/userModel");
-
+const bcrypt=require("bcryptjs");
+const jwt=require("jsonwebtoken");
 const registerController=async(req,res)=>{
     try{
         const{username,email,password}=req.body;
@@ -18,8 +19,12 @@ const registerController=async(req,res)=>{
                 message:"User already exists"
             })
         }
+        //hash password
+        //more the salt rounds more secure the password but it will take more time.
+        const salt=await bcrypt.genSalt(10);
+        const hashedPassword=await bcrypt.hash(password,salt);
         //save user 
-        const newUser=new userModel({username,email,password})
+        const newUser=new userModel({username,email,password:hashedPassword})
         await newUser.save();
         res.status(201).send({
             success:true,
@@ -53,11 +58,24 @@ const loginController=async(req,res)=>{
                 message:"User not found"
             })
         }
-
+        //compare password
+        const isMatch=await bcrypt.compare(password,user.password);
+        if(!isMatch){
+            return res.status(400).send({
+                success:false,
+                message:"Invalid password"
+            })
+        }
+        const token=await jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:"1d"});
         res.status(200).send({
             success:true,
             message:"login successfully",
-            user
+            token,
+            user:{
+                id:user._id,
+                username:user.username,
+                email:user.email
+            }
         })
     }catch(error){
         console.log(error);
